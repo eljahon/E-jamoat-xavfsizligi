25e6a00de9df365a946fd6d8d665c54a70cb6710
oybek1024 <mukhiddinov.oybek1024@gmail.com>
2021-03-13 21:01:59 +0500

    index updated

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -9,7 +9,7 @@ export default {
   menu: {
     locale: true
   },
-  title: 'Zaytun Admin',
+  title: 'Zaytun',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
